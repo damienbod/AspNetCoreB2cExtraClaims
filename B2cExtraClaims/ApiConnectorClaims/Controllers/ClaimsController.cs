1f72a549a14e44f3b9f87aab38a9b0faaf67d67d
damienbod <damien_bod@hotmail.com>
2021-11-12 23:15:44 +0100

    Adding some validation

--- B2cExtraClaims/ApiConnectorClaims/Controllers/ClaimsController.cs
+++ B2cExtraClaims/ApiConnectorClaims/Controllers/ClaimsController.cs
@@ -21,12 +21,25 @@ namespace ApiConnectorClaims.Controllers
             string content = await new System.IO.StreamReader(Request.Body).ReadToEndAsync();
             var requestConnector = JsonSerializer.Deserialize<RequestConnector>(content);
 
+            // If input data is null, show block page
+            if (requestConnector == null)
+            {
+                return Ok(new ResponseContent("ShowBlockPage", "There was a problem with your request."));
+            }
+
+            // If email claim not found, show block page. Email is required and sent by default.
+            if (requestConnector.Email == null || requestConnector.Email == "" || requestConnector.Email.Contains("@") == false)
+            {
+                return Ok(new ResponseContent("ShowBlockPage", "Email name is mandatory."));
+            }
+
             // TODO verify requestConnector
-            var result = new ResponseContent();
+            var result = new ResponseContent
+            {
+                // use the objectId of the email to get the user specfic claims
+                MyCustomClaim = $"everything awesome {requestConnector.Email}"
+            };
 
-            // use the objectId of the email to get the user specfic claims
-
-            result.MyCustomClaim = $"everything awesome {requestConnector.Email}";
             return Ok(result);
         }
     }
