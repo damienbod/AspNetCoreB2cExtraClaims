0fcb8ec022b46357afc2c0c45860189d6ca61979
damienbod <damien_bod@hotmail.com>
2021-11-18 14:49:56 +0100

    Adding some certificate auth stuff

---
+++ ApiConnectorClaims/MyCertificateValidationService.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ApiConnectorClaims
+{
+    public class MyCertificateValidationService 
+    {
+        private readonly ILogger<MyCertificateValidationService> _logger;
+
+        public MyCertificateValidationService(ILogger<MyCertificateValidationService> logger)
+        {
+            _logger = logger;
+        }
+
+        public bool ValidateCertificate(X509Certificate2 clientCertificate)
+        {
+            return CheckIfThumbprintIsValid(clientCertificate);
+        }
+
+        private bool CheckIfThumbprintIsValid(X509Certificate2 clientCertificate)
+        {
+            var listOfValidThumbprints = new List<string>
+            {
+                // add thumprints of your allowed clients
+                "15D118271F9AE7855778A2E6A00A575341D3D904"
+            };
+
+            if (listOfValidThumbprints.Contains(clientCertificate.Thumbprint))
+            {
+                _logger.LogInformation($"Custom auth-success for certificate  {clientCertificate.FriendlyName} {clientCertificate.Thumbprint}");
+
+                return true;
+            }
+
+            _logger.LogWarning($"auth failed for certificate  {clientCertificate.FriendlyName} {clientCertificate.Thumbprint}");
+
+            return false;
+        }
+    }
+}
---
+++ AzureCertAuthClientConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace AzureCertAuthClientConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Get data!");
+
+            var payload = await GetApiDataUsingHttpClientHandler();
+
+            Console.WriteLine("Success!");
+        }
+
+        private static async Task<string> GetApiDataUsingHttpClientHandler()
+        {
+            var cert = new X509Certificate2("client.pfx", "1234");
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(cert);
+            var client = new HttpClient(handler);
+
+            var request = new HttpRequestMessage()
+            {
+                // You need to configure your server auth for this
+                RequestUri = new Uri("https://apiconnectorclaims20211118132813.azurewebsites.net/api/Claims"),
+                Method = HttpMethod.Get,
+            };
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseContent);
+                return responseContent;
+            }
+
+            throw new ApplicationException($"Status code: {response.StatusCode}, Error: {response.ReasonPhrase}");
+        }
+    }
+}
